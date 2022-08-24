5c60ab1f157aa496011961a28faa288392beceb4
Trần Đỗ Anh Khoa <anhkhoatrando123@gmail.com>
2022-08-24 22:28:13 +0700

    fix more bugs

--- main.js
+++ main.js
@@ -9,5 +9,22 @@ function activeMenu() {
     if (len >= 0) li[len].classList.add("active");
 }
 
+function scrollHeader(section) {
+    if (screen.width <= 768) {
+        document.documentElement.scrollTop = document.getElementById(section).offsetTop - 100;
+    } else {
+        document.getElementById(section).scrollIntoView();
+    }
+}
+
+function detectTopButton() {
+    if (document.documentElement.scrollTop > 500) {
+        topButton.style.display = "block";
+    } else {
+        topButton.style.display = "none";
+    }
+}
+
 activeMenu();
 window.addEventListener("scroll", activeMenu);
+window.addEventListener("scroll", detectTopButton);
