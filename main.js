77fe155698678878aca33152f55476f54dacc960
Trần Đỗ Anh Khoa <anhkhoatrando123@gmail.com>
2022-08-24 19:53:50 +0700

    done

---
+++ main.js
@@ -0,0 +1,13 @@
+const li = document.querySelectorAll(".nav-item");
+const sec = document.querySelectorAll(".content-list-item");
+
+function activeMenu() {
+    let cur = document.documentElement.scrollTop;
+    let len = sec.length;
+    while (len-- && cur + 50 < sec[len].offsetTop) {}
+    li.forEach((list) => list.classList.remove("active"));
+    if (len >= 0) li[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll", activeMenu);
